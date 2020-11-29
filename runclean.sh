d2faeaf6abb604be797daedd7d510135af199bcd
= <=>
2020-11-29 14:03:31 +1100

    fixed paths in scripts

--- runclean.sh
+++ runclean.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-sudo rm -rf /home/ross/rossgit/toolfactory_docker/export/*
-sudo rm -rf /home/ross/rossgit/toolfactory_docker/export/.distribution_config
-sudo cp post-start-actions.sh  /home/ross/rossgit/toolfactory_docker/export/
+sudo rm -rf export/*
+sudo rm -rf export/.distribution_config
+sudo cp post-start-actions.sh  export/
 sudo cp tfwelcome.html export/
-#sudo chmod a+x /home/ross/rossgit/toolfactory_docker/export/
 docker build -t quay.io/fubar2/toolfactory-galaxy-docker  .
-#sudo cp post-start-actions.sh /home/ross/rossgit/toolfactory_docker/export/
-#Cannot get that to work.
-#sudo chmod a+x /home/ross/rossgit/toolfactory_docker/export/post-start-actions.sh
 ./starttoolfactory.sh
 
