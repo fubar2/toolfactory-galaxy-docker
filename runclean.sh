f7ee338c482a93dff899561e72e52107a06a936c
= <=>
2020-11-29 14:12:55 +1100

    more path fixes for runner scripts

--- runclean.sh
+++ runclean.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo rm -rf export/*
 sudo rm -rf export/.distribution_config
-sudo cp post-start-actions.sh  export/
-sudo cp tfwelcome.html export/
+sudo cp files/post-start-actions.sh  export/
+sudo cp files/tfwelcome.html export/
 docker build -t quay.io/fubar2/toolfactory-galaxy-docker  .
 ./starttoolfactory.sh
 
--- rundirty.sh
+++ rundirty.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-docker build -t quay.io/fubar2/toolfactory-galaxy-docker .
+sudo rm export/post-start-actions.sh
+#docker build -t quay.io/fubar2/toolfactory-galaxy-docker .
 ./starttoolfactory.sh
 
