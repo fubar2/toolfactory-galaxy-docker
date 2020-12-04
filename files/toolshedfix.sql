005014231992cf0b1b50aaab41bc486ae27ffc26
= <=>
2020-12-04 14:06:30 +1100

    adding some sql to fixup toolshed database - none of the utility scripts seem to work as they are .ini based - years out of date :(

---
+++ files/toolshedfix.sql
@@ -0,0 +1,3 @@
+INSERT INTO galaxy_user VALUES(1,'2020-12-04 02:56:35.066506','2020-12-04 02:56:35.066519','admin@galaxy.org','fubar','fakekey',0,0,0,0);
+INSERT INTO role VALUES(2,'2020-12-04 02:56:35.083902','2020-12-04 02:56:35.083909','admin@galaxy.org','Private Role for admin@galaxy.org','private',0);
+INSERT INTO user_role_association VALUES(1,1,2,'2020-12-04 02:56:35.097627','2020-12-04 02:56:35.097634');
