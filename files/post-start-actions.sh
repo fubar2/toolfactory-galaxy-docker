b0b697bb2ca0af42c0c2d94f2eca68562ba1e9cc
= <=>
2020-12-25 15:21:18 +1100

    missing space in post-start-actions line

--- files/post-start-actions.sh
+++ files/post-start-actions.sh
@@ -5,7 +5,7 @@ OLDPATH=$PATH
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 /galaxy_venv/bin/python3 -m venv /export/tfvm && /export/tfvm/bin/python3 -m pip install ephemeris bioblend requests
 PATH=/export/tfvm/bin:$PATH
-/export/tfvm/bin/workflow-install --workflow_path $GALAXY_ROOT/config/workflows/ -g http://localhost:8080-a $GALAXY_DEFAULT_ADMIN_KEY
+/export/tfvm/bin/workflow-install --workflow_path $GALAXY_ROOT/config/workflows/ -g http://localhost:8080 -a $GALAXY_DEFAULT_ADMIN_KEY
 /export/tfvm/bin/python3 /usr/local/bin/install-history.py -a $GALAXY_DEFAULT_ADMIN_KEY -i $GALAXY_ROOT/config/histories/TFhistory.tar.gz -g http://localhost:8080
 /export/tfvm/bin/shed-tools install -g http://localhost:8080 -a $GALAXY_DEFAULT_ADMIN_KEY -t $GALAXY_ROOT/config/tools/TFtools.yml
 /export/tfvm/bin/python3 /usr/local/bin/hackadmin.py -d $GALAXY_ROOT/database/community.sqlite
